5ec7c66d3271077dbc678f9165825ef9909edcac
Budiarta <iniyin187@gmail.com>
2025-06-06 13:53:17 +0800

    galeri sudah jadi

--- JS/script.js
+++ JS/script.js
@@ -111,10 +111,13 @@ filterButtons.forEach(button => {
     // GALERI PUNYA
 new Swiper('.card-wrapper', {
   loop: true,
+    spaceBetween: 30,
 
-  // If we need pagination
+  // Pagination bullets
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
   },
 
   // Navigation arrows
@@ -122,4 +125,17 @@ new Swiper('.card-wrapper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+   // Responsive brakpoints
+  breakpoints:{
+    0: {
+        slidesPerView: 1
+    },
+     768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
 });
